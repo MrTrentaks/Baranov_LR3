42f15c89c4877d197c102400d7c285da416a4666
2Д - Максим Баранов <baranov@VSPC34.RU>
2021-05-19 18:19:01 +0300

    Baranov

---
+++ Baranov_LR3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+    cout << "Создание списка";
+    int spisok[10];
+    for (int i = 0; i < size(spisok); i++)
+    {
+        spisok[i] = rand() % 21 - 10;
+    }
+    vector<int> minus;
+    vector<int> plus;
+    for (int i = 0; i < size(spisok); i++)
+    {
+        if (spisok[i] < 0)
+        {
+            minus.push_back(spisok[i]);
+        }
+        else
+            plus.push_back(spisok[i]);
+    }
+    vector<int>::iterator Iter;
+    cout << " Отрицательные:\n";
+    for (Iter = minus.begin(); Iter != minus.end(); Iter++)
+    {
+        cout << *Iter << endl;
+    }
+    cout << "\n Положительные:\n";
+    for (Iter = plus.begin(); Iter != plus.end(); Iter++)
+    {
+        cout << *Iter << endl;
+    }
+}
+
